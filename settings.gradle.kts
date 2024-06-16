793ddf4b5accea906e4299fb0340f6283fb74417
Ryzhov <dryzhov@neoflex.ru>
2024-06-16 21:37:54 +0300

    hello message was changed

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "RDA6666"
+
---
+++ src/main/java/datatech/nflxdryzhov/Main.java
@@ -0,0 +1,7 @@
+package datatech.nflxdryzhov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello QA GURURRRR!");
+    }
+}
